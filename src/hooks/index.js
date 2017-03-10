5fc91b2d90d9303f9c8032f30cd7945011345700
mirandaflu <randaflu@gmail.com>
2017-03-10 00:44:37 -0700

    lock down editing of collections, fields, things, and attributes

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,104 @@
 'use strict';
 
 // Add any common hooks you want to share across services in here.
-//
-// Below is an example of how a hook is written and exported. Please
-// see http://docs.feathersjs.com/hooks/readme.html for more details
+// See http://docs.feathersjs.com/hooks/readme.html for more details
 // on hooks.
 
-exports.myHook = function(options) {
+const allowEditBasedOnWorkspace = result => {
+	return new Promise((resolve, reject) => {
+		let workspace = result.workspace, userID = result.userID,
+			service = result.hook.service.Model.modelName, method = result.hook.method;
+		let owner = workspace.owner.toString(),
+			admins = workspace.admins.map((o) => o.toString()),
+			editors = workspace.editors.map((o) => o.toString());
+		if (userID != owner && admins.indexOf(userID) == -1 && editors.indexOf(userID) == -1) {
+			reject(new Error('You are not allowed to '+method+' a '+service+' in this workspace.'));
+		}
+		else {
+			resolve();
+		}
+	});
+};
+
+exports.allowWorkspaceEditorOrHigher = function(options) {
 	return function(hook) {
-		console.log('My custom global hook ran. Feathers is awesome!');
+		return new Promise((resolve, reject) => {
+			if (!hook.params.provider) {
+				return resolve(hook);
+			}
+			let userID = hook.params.user._id.toString();
+			if (hook.service.Model.modelName == 'collection') {
+				if (hook.method == 'create') {
+					hook.app.service('workspaces')
+						.get(hook.data.workspace)
+						.then(workspace => { return {
+							workspace: workspace,
+							userID: userID,
+							hook: hook
+						}; })
+						.then(allowEditBasedOnWorkspace)
+						.then(result => { resolve(hook); })
+						.catch(reject);
+				}
+				else if (hook.id) {
+					hook.service.get(hook.id).then(collection => {
+						hook.app.service('workspaces')
+							.get(collection.workspace)
+							.then(workspace => { return {
+								workspace: workspace,
+								userID: userID,
+								hook: hook
+							}; })
+							.then(allowEditBasedOnWorkspace)
+							.then(result => { resolve(hook); })
+							.catch(reject);
+					}).catch(reject);
+				}
+				else reject(new Error('Unknown error occurred while checking permissions'));
+			}
+			else if (['attribute','field','thing'].indexOf(hook.service.Model.modelName)) {
+				if (hook.method == 'create') {
+					hook.app.service('collections').get(hook.data.coll).then(collection => {
+						hook.app.service('workspaces')
+							.get(collection.workspace)
+							.then(workspace => { return {
+								workspace: workspace,
+								userID: userID,
+								hook: hook
+							}; })
+							.then(allowEditBasedOnWorkspace)
+							.then(result => { resolve(hook); })
+							.catch(reject);
+					}).catch(reject);
+				}
+				else if (hook.id) {
+					hook.service.get(hook.id).then(result => {
+						hook.app.service('collections').get(result.coll).then(collection => {
+							hook.app.service('workspaces')
+								.get(collection.workspace)
+								.then(workspace => { return {
+									workspace: workspace,
+									userID: userID,
+									hook: hook
+								}; })
+								.then(allowEditBasedOnWorkspace)
+								.then(result => { resolve(hook); })
+								.catch(reject);
+						}).catch(reject);
+					}).catch(reject);
+				}
+				else reject(new Error('Unknown error occurred while checking permissions'));
+			}
+			else reject(new Error('Unknown error occurred while checking permissions'));
+		});
+	};
+};
+
+exports.mustProvideCollection = function(options) {
+	return function(hook) {
+		return new Promise((resolve, reject) => {
+			if (!hook.params.query.coll && !hook.data.coll) reject(new Error('You must specify a collection to '+hook.method+' '+hook.service.Model.modelName+'s.'));
+			else resolve(hook);
+		});
 	};
 };
