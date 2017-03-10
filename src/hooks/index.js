01b6655290f5ab58b44e620562975e7aba598e3f
mirandaflu <randaflu@gmail.com>
2017-03-10 14:41:58 -0700

    fixed field/thing/attribute edit permissions issue from last update

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -56,7 +56,7 @@ exports.allowWorkspaceEditorOrHigher = function(options) {
 				}
 				else reject(new Error('Unknown error occurred while checking permissions'));
 			}
-			else if (['attribute','field','thing'].indexOf(hook.service.Model.modelName)) {
+			else if (['attribute','field','thing'].indexOf(hook.service.Model.modelName) != -1) {
 				if (hook.method == 'create') {
 					hook.app.service('collections').get(hook.data.coll).then(collection => {
 						hook.app.service('workspaces')
