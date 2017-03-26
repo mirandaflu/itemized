4ba9edcc77944d5653572770a3ba58eecadde2aa
mirandaflu <randaflu@gmail.com>
2017-03-26 03:15:40 -0600

    fix issue that blocked deleting the last option in a single select field
    in the field configuration modal

--- app/components/configurefield.jsx
+++ app/components/configurefield.jsx
@@ -44,7 +44,7 @@ class ConfigureField extends React.Component {
 			});
 	}
 	handleOptionsSelectChange = (property, values) => {
-		let value = values.value;
+		let value = [];
 		if (values.length) value = values.map(v => { return v.value; });
 		const s = {};
 		s[property] = value;
@@ -106,9 +106,6 @@ class ConfigureField extends React.Component {
 		for (const t in fieldTypes) {
 			typeOptions.push({value: t, label: t});
 		}
-		for (const o of field.options) {
-			optionOptions.push({value: o, label: o});
-		}
 		const FieldComponent = (field.type) ?
 			fieldTypes[field.type].component : fieldTypes.Static.component;
 
@@ -160,7 +157,12 @@ class ConfigureField extends React.Component {
 									<label htmlFor="options">Options</label>
 									<Select.Creatable
 										id="options"
-										value={optionOptions}
+										value={field.options.map(option => {
+											return {
+												label: option,
+												value: option
+											};
+										})}
 										options={[]}
 										multi
 										onChange={this.handleOptionsSelectChange.bind(this, 'options')} />
